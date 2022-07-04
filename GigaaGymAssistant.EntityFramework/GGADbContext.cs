b53b244a033898e5d9569559b1e0ad72e1007343
Karol Angrys <karol.angrys@gmail.com>
2022-07-04 18:08:27 +0200

    database created

---
+++ GigaaGymAssistant.EntityFramework/GGADbContext.cs
@@ -0,0 +1,29 @@
+﻿using GigaaGymAssistant.Infrastructure.EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace GigaaGymAssistant.Infrastructure.EntityFramework;
+
+public class GGADbContext : DbContext
+{
+    private readonly IConfiguration _config;
+
+    public GGADbContext(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .Property(u => u.Email)
+            .IsRequired();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
+    }
+}
